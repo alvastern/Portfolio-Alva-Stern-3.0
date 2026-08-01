1e804a610f13c98c1fe66e9884a99eff687b083f
alvastern <alva.stern02@gmail.com>
2026-08-01 23:00:41 +0200

    Styling av projekt - pizza basilico

--- src/app/pages/dizparc/dizparc.ts
+++ src/app/pages/dizparc/dizparc.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
-import { Footer } from "../../components/footer/footer";
+import { Footer } from '../../components/footer/footer';
 
 @Component({
   selector: 'app-dizparc',
-  imports: [RouterLink, Footer],
+  imports: [CommonModule, RouterLink, Footer],
   standalone: true,
   templateUrl: './dizparc.html',
   styleUrl: './dizparc.scss',
 })
-export class Dizparc {}
+export class Dizparc {
+  selectedImage: string | null = null;
+
+  openImage(imageSrc: string): void {
+    this.selectedImage = imageSrc;
+    document.body.style.overflow = 'hidden';
+  }
+
+  closeImage(): void {
+    this.selectedImage = null;
+    document.body.style.overflow = '';
+  }
+}
--- src/app/pages/pizza-basilico/pizza-basilico.ts
+++ src/app/pages/pizza-basilico/pizza-basilico.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
-import { Footer } from "../../components/footer/footer";
+import { Footer } from '../../components/footer/footer';
 
 @Component({
-  selector: 'app-pizza-basilico',
-  imports: [RouterLink, Footer],
+  selector: 'app-dizparc',
+  imports: [CommonModule, RouterLink, Footer],
   standalone: true,
   templateUrl: './pizza-basilico.html',
   styleUrl: './pizza-basilico.scss',
 })
-export class PizzaBasilico {}
+export class Dizparc {
+  selectedImage: string | null = null;
+
+  openImage(imageSrc: string): void {
+    this.selectedImage = imageSrc;
+    document.body.style.overflow = 'hidden';
+  }
+
+  closeImage(): void {
+    this.selectedImage = null;
+    document.body.style.overflow = '';
+  }
+}
